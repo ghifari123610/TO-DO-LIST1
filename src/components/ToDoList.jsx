fa6c70be955a9b3c58047dff4554583856c6ba98
ghifari123610 <ghifaripondokit@gmail.com>
2024-11-07 16:31:12 +0800

    first commit

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,34 @@
+import ToDoListbutton from "./ToDoListbutton"
+function ToDoList(props) {
+  props.tasks.sort((a, b)=> b.id - a.id)
+  return (
+    <div className="wrapper">
+      <ul>
+        {
+           props.tasks.map((item)=>{
+            let radioCompleted = '';
+            let classCompleted = '';
+            if(item.completed == false){
+              radioCompleted = '◻️';
+            } else {
+              radioCompleted = '✅';
+              classCompleted = 'strike';
+            }
+            return(
+              <li key={item.id}>
+                  <div className='left'><button onClick={()=> props.setCompleted(item.id)}>{radioCompleted}</button></div>
+                  <div className={`center ${classCompleted}`}>{item.task}</div>
+                  <div className='right'>
+                      <ToDoListbutton id={item.id} tasks={props.tasks} move={props.move} remove={props.remove}/>
+                  </div>
+              </li>
+            )
+           })
+        }
+        
+      </ul>
+    </div>
+  )
+}
+
+export default ToDoList
